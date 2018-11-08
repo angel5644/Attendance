d6ceb2e10e10bbbb75d4d052317895667dab54c4
Yessica Zarate Olvera <yessica.zarate@4thsource.com>
2018-11-08 10:41:51 -0600

    Create and Edit View Models added.

---
+++ Attendance/Attendance/ViewModels/CreateEmployeeVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Attendance.Enums;
+
+namespace Attendance.ViewModels
+{
+    public class CreateEmployeeVM
+    {
+        
+        [Required]
+        [StringLength (500)]
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+        public string EMail { get; set; }
+                     
+    }
+}
---
+++ Attendance/Attendance/ViewModels/EditEmployeeVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Attendance.Enums;
+
+namespace Attendance.ViewModels
+{
+    public class EditEmployeeVM
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        [StringLength(500)]
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+        public string EMail { get; set; }
+    }
+}
