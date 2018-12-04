cad5ebc9f28524c460fe78e1e297ef20c8dd6fca
Angemonium <44067283+Angemonium@users.noreply.github.com>
2018-12-04 10:45:00 -0600

    Edit and Delete Completed - EnglishClass

---
+++ Attendance/Attendance/ViewModels/EnglisClass/EditEnglishVM.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Attendance.Enums;
+using Attendance.Models;
+using System.Web.Mvc;
+namespace Attendance.ViewModels.EnglisClass
+{
+    public class EditEnglishVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Class Name")]
+        [Required]
+        [StringLength(500)]
+        public string Name { get; set; }
+
+        [Display(Name = "Teacher's Name")]
+        [Required]
+        public int EmployeeId { get; set; }
+        public IEnumerable<SelectListItem> TName { get; set; }
+
+        [Display(Name = "Location's Name")]
+        [Required]
+        public int LocationId { get; set; }
+        public IEnumerable<SelectListItem> LName { get; set; }
+
+        [Display(Name = "Group's Name")]
+        [Required]
+        public int GroupId { get; set; }
+        public IEnumerable<SelectListItem> GName { get; set; }
+
+        [Display(Name = "Monday")]
+        public bool IsMonday { get; set; }
+
+        [Display(Name = "Tuesday")]
+        public bool IsTuesday { get; set; }
+
+        [Display(Name = "Wednesday")]
+        public bool IsWednesday { get; set; }
+
+        [Display(Name = "Thursday")]
+        public bool IsThursday { get; set; }
+
+        [Display(Name = "Friday")]
+        public bool IsFriday { get; set; }
+
+        [Display(Name = "Starts at")]
+        [Required]
+        [Range(0, 23)]
+        public int HourStart { get; set; }
+
+        [Display(Name = "Ends at")]
+        [Required]
+        [Range(0, 23)]
+        public int HourEnd { get; set; }
+
+        //[Required]
+        public DateTimeOffset? DateUpdated { get; set; }
+
+        //[Display(Name = "User Upddated")]
+        //
+
+        public string UserUpdated { get; set; }
+    }
+}
