0877f6c4cd57ba062c6177acf0eef8779b93f0fc
RayJair22 <44067242+RayJair22@users.noreply.github.com>
2018-12-05 14:16:18 -0600

    Enrollments Details View completed!

--- Attendance/Attendance/ViewModels/Enrollment/DetailsEnrollmentVM.cs
+++ Attendance/Attendance/ViewModels/Enrollment/DetailsEnrollmentVM.cs
@@ -23,18 +23,21 @@ namespace Attendance.ViewModels.Enrollment
         public string ClassName { get; set; }
 
         [Display(Name = "Enrollment Date")]
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
         public DateTimeOffset DateEnrollment { get; set; }
 
         [Display(Name = "Note")]
         public string Notes { get; set; }
 
         [Display(Name = "Date created")]
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
         public DateTimeOffset? DateCreated { get; set; }
 
         [Display(Name = "User Created")]
         public string UserCreated { get; set; }
 
         [Display(Name = "Date Updated")]
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
         public DateTimeOffset? DateUpdated { get; set; }
 
         [Display(Name = "User Upddated")]
